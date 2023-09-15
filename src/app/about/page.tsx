cc45a4c7238041d7f9d9f840cde0a09f946ba071
Gabriel R. Oliveira <gabriel7r.oliveira@gmail.com>
2023-09-15 18:39:15 -0300

    Componentes página inicial

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Courts from "./components/Courts";
+
+export const metadata = {
+    title: "About",
+};
+
+const Page = () =>{
+    return (<div>
+    <Courts />
+    </div>
+    );
+};
+
+export default Page;
